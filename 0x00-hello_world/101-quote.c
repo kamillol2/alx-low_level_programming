12bfb7f790484aeff10e5e595fef88ca8f83b0e2
kamillol2 <kamil69ali@gmail.com>
2023-10-06 09:09:25 -0700

    name error fixed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ *main-printing on sterr without
+ *printf or put
+ *
+ *Return: 1
+ */
+int main(void)
+{
+	char *line_to_print = "and that piece of art is useful\" -\
+			       Dora Korpar, 2015-10-19";
+
+	fwrite(line_to_print, sizeof(char), strlen(line_to_print), stderr);
+	return (1);
+}
