d62e395c47f2e4cfe0dfe342c3ce4694ee0ebd6a
kamillol2 <kamil69ali@gmail.com>
2023-10-06 03:37:58 -0700

    adjustement v1.0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	"Size of an int: 4 byte(s)\n"
 	"Size of a long int: 4 byte(s)\n"
 	"Size of a long long int: 8 byte(s)\n"
-	"Size of a float: 4 byte(s)");
+	"Size of a float: 4 byte(s)\n");
 	return (0);
 }
