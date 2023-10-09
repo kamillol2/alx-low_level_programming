3dd9358a187163a843ee67e6aa00a59d732d50c2
kamillol2 <125408220+kamillol2@users.noreply.github.com>
2023-10-09 22:54:46 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 	for (loop = 48 ; loop < 58; loop++)
 	{
 		for (loop_in = loop + 1 ; loop_in < 58; loop_in++)
-		{	
+		{
 			putchar(loop);
 			putchar(loop_in);
 			if (loop != 56)
