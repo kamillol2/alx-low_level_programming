4ae04492fe1073afccc4e4985bc65d1772431723
kamillol2 <kamil69ali@gmail.com>
2023-10-12 10:10:41 +0100

    fixing spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,15 +19,17 @@ void times_table(void)
 			{
 				_putchar('0' + (prod / 10));
 				_putchar('0' + (prod % 10));
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(prod / 10);
 				_putchar('0' + (prod % 10));
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
