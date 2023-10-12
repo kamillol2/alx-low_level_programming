55be0955eeeea3922e1b5ef1d8d50d49f0a345ec
kamillol2 <kamil69ali@gmail.com>
2023-10-12 08:44:34 +0100

    adding the prototypes to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,13 @@ int _isalpha(int c);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
+
+int print_last_digit(int a);
+
+int _abs(int a);
+
+int _print_sign(int n);
+
+int _isalpha(int c);
+
+int _islower(int c);
