8ed1b008c3cb238b0e1534d0d50b0ea5f8173252
kamillol2 <kamil69ali@gmail.com>
2023-10-12 11:12:34 +0100

    adding if to take care of extra char

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,30 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
+			if (n != 98)
+			{
 			printf("%d ,", n);
 			n--;
+			}
+			else
+			{
+				printf("%d", n);
+			}
 		}
 	}
 	else if (n < 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
+			if (n != 98)
+			{
 			printf("%d ,", n);
-			n++;
+			n--;
+			}
+			else
+			{
+				printf("%d", n);
+			}
 		}
 	}
 }
