710a212398d868502a7a614016071e2b60b3905d
kamillol2 <kamil69ali@gmail.com>
2023-10-12 11:17:37 +0100

    adding new line at the end \n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,8 +18,9 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d\n", n);
 				n--;
+
 			}
 		}
 	}
@@ -34,7 +35,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d\n", n);
 				n++;
 			}
 		}
