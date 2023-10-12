7922843d70599ecc59ba03a395ddbaba506daad1
kamillol2 <kamil69ali@gmail.com>
2023-10-12 11:14:22 +0100

    fixing infinite loop problem

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,12 +13,13 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-			printf("%d ,", n);
-			n--;
+				printf("%d ,", n);
+				n--;
 			}
 			else
 			{
 				printf("%d", n);
+				n--;
 			}
 		}
 	}
@@ -28,12 +29,13 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-			printf("%d ,", n);
-			n--;
+				printf("%d ,", n);
+				n++;
 			}
 			else
 			{
 				printf("%d", n);
+				n++;
 			}
 		}
 	}
