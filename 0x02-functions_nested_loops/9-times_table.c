b7bd9964052dda1b62baec30efcaad974c2e539f
kamillol2 <kamil69ali@gmail.com>
2023-10-12 10:15:29 +0100

    fixing spaces around 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			prod = i * j;
-			if (prod >= 10)
+			if (prod > 10)
 			{
 				_putchar('0' + (prod / 10));
 				_putchar('0' + (prod % 10));
