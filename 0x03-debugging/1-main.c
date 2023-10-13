819de6e9c0dae237e1b44e806c5e4bcb525885cc
kamillol2 <kamil69ali@gmail.com>
2023-10-13 19:07:16 +0100

    adding comment with i++

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)/*No incrementation of i inside the While loop*/
+	while (i < 10)
 	{
-		putchar(i); i++;
+		putchar(i); /*i++*/
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
