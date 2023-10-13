20565a5a690925c91bce01305fdb9a24f4de49ad
kamillol2 <kamil69ali@gmail.com>
2023-10-14 00:14:40 +0100

    commiting the test room to sync

--- testing-room/main-test-room.h
+++ testing-room/main-test-room.h
@@ -43,11 +43,11 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		return ('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		_putchar('_');
 	_putchar('\n');
 	}
