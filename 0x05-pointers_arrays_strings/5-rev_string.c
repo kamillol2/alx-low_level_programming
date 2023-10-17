de5ede3e79de7a885f15836e8eb49f55a08fa022
kamillol2 <kamil69ali@gmail.com>
2023-10-17 22:12:27 +0100

    gpt fix wiht a for loop and a temp

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,18 @@
-#include"main.h"
+#include "main.h"
 /**
- *rev_string-reverse a string and put it back into s
- *@s: the string to reverse and stock the new string
- *
- *
-*/
+ * rev_string - reverse a string and store it back in s
+ * @s: the string to reverse
+ */
 void rev_string(char *s)
 {
 	int size = strlen(s);
+	int i;
+	char temp;
 
-	int i = 0;
-
-	while (size >= 0)
+	for (i = 0; i < size / 2; i++)
 	{
-		s[i] = s[size - 1];
-		size--;
-		i++;
+		temp = s[i];
+		s[i] = s[size - i - 1];
+		s[size - i - 1] = temp;
 	}
 }
-
