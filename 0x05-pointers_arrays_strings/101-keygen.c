f532f25a33dffb1c5e2fcf3ee2906ddeda2a6a2e
kamillol2 <kamil69ali@gmail.com>
2023-10-18 20:26:18 +0100

    extra tasks completed

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * main - Generate a random password for 101-crackme file
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int password[64], i, sum = 0, n;
+
+	srand(time(NULL));
+	for (i = 0; i < 64; i++)
+	{
+		password[i] = rand() % 78;
+		sum += password[i] + '0';
+		putchar(password[i] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
