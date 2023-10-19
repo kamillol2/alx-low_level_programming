b8419726afc6a25dbc206c6b20554100f8862e05
kamillol2 <kamil69ali@gmail.com>
2023-10-19 15:26:07 +0100

    fixing start point and adding the output for test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,24 @@ char *_strcat(char *dest, char *src)
 
 	int i;
 
-	for (i = len_dest; i <= (len_src + len_dest); i++)
-		dest[i] = src[i];
-	return (*dest);
+	int p = 0;
+
+	for (i = len_dest; i <= (len_src + len_dest); i++, p++)
+		dest[i] = src[p];
+
+	return (dest);
+}
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
