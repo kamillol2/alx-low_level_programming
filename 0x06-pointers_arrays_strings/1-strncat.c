eaacd1b1817e79d6ba6dec296c3cd52239e0610f
kamillol2 <kamil69ali@gmail.com>
2023-10-20 17:40:55 +0100

    trying smth

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,22 @@
 /**
- * _strncat - main function
- *
- * Description: 'Concatenate two strings'
- *
+ * _strncat-Concatenate two strings
  * @src: the string to be copied
- * @dest: the resulte
- * @n: number of character to coy
+ * @dest: the resulting string
+ * @n: number of character to copy
  *
  * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, length = 0;
+	int i,
+	
+	length = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	{
 		length++;
-	}
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[length + i] = src[i];
-	}
 	dest[length + i] = '\0';
 	return (dest);
 }
