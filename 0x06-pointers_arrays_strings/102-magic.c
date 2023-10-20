ffd31763fd75c80ce8599a4bae81ede20c9579f6
kamillol2 <kamil69ali@gmail.com>
2023-10-20 18:16:32 +0100

    trying smth

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,6 +4,8 @@
  * followed by a new line.
  * Return: 0
 */
+#include <stdio.h>
+
 int main(void)
 {
 	int n;
@@ -12,7 +14,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+	/*
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
 	*(p + (a + 2) - (p + 2)) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
