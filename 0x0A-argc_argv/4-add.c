9b873375d968c64e94574da4c14d1f90ece7135b
kamillol2 <kamil69ali@gmail.com>
2023-10-30 18:49:03 +0100

    compilation problems

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,20 +8,18 @@
 */
 int main(int argc, char *argv[])
 {
+	int sum;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int i;
-
 	if (atoi(argv[1]) == 0 || atoi(argv[1]) == 0)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int sum;
-
 	sum = atoi(argv[1]) + atoi(argv[2]);
 	printf("%d\n", sum);
 	return (0);
