75e0984a20813b2d966ccd8f89172d876ddc02b8
kamillol2 <kamil69ali@gmail.com>
2023-10-30 18:38:19 +0100

    fixing compilation error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,13 @@
 */
 int main(int argc, char *argv[])
 {
+	int prod;
+
 	if (argc != 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
-	int prod;
-
 	prod = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", prod);
 	return (0);
