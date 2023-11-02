bf4c08b74672371039a47fee58a802974a01dbee
kamillol2 <kamil69ali@gmail.com>
2023-11-02 09:49:55 +0100

    adding lib

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<string.h>
+#include<stdlib.h>
 /**
  * argstostr - Concatenates all the arguments of the program in a new string.
  *
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<string.h>
+#include<stdlib.h>
 /**
  * strtow - Splits a string into array of words.
  *
