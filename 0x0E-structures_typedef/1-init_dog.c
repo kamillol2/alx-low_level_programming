33fe65f29e64a947ca39634390f840f8afd24082
kamillol2 <kamil69ali@gmail.com>
2023-11-06 16:00:13 +0100

    fixing order

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include"dog.h"
+#include<stdio.h>
 /**
  *init_dog- initialize the struct dog
  *@d: pointer to struct
@@ -12,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->age = age;
 		d->name = name;
+		d->age = age;
 		d->owner = owner;
 	}
 }
