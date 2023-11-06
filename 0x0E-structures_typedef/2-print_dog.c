bf3b1ab157148f67a6c28e7331e16e7c91587a90
kamillol2 <kamil69ali@gmail.com>
2023-11-06 15:56:48 +0100

    ADDING null check

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,21 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
+	if (d != NULL)
 	{
-		d->name = "(nil)";
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
+		if (d->age <= 0)
+		{
+			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+			exit(0);
+		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
-	if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
-	if (d->age <= 0)
-	{
-		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-		exit(0);
-	}
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (new_dog != NULL)
 	{
-		return (NULL);
+		new_dog->age = age;
+		new_dog->name = name;
+		new_dog->owner = owner;
+		return (new_dog);
 	}
-	new_dog->age = age;
-	new_dog->name = name;
-	new_dog->owner = owner;
-	return (new_dog);
+	return (NULL);
 }
